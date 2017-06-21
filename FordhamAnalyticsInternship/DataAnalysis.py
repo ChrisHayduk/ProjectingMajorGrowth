2549497076853dd803522d4f046c25621007da6d
ChrisHayduk <chayduk@fordham.edu>
2017-06-21 16:51:57 -0400

    End of day commit

--- FordhamAnalyticsInternship/DataAnalysis.py
+++ FordhamAnalyticsInternship/DataAnalysis.py
@@ -1,6 +1,7 @@
 import pandas as pd, sklearn, numpy, matplotlib.pyplot as plt, statsmodels
+from datetime import datetime
 
-CleanedData = pd.read_csv('CleanedData.csv', index_col=2, low_memory=False) #Change term code column to date time when possible
+CleanedData = pd.read_csv('CleanedData.csv', index_col=2, low_memory=False, parse_dates=['DATE']) #Change term code column to date time when possible
 del CleanedData['Unnamed: 0']
 
 majors = CleanedData['MAJOR_CODE_1'].unique().tolist()
