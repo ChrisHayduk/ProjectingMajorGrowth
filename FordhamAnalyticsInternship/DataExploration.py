d9f666e442ebef46bb146c257e0c25c692a1d19c
ChrisHayduk <chayduk@fordham.edu>
2017-07-11 17:10:08 -0400

    Updates

--- FordhamAnalyticsInternship/DataExploration.py
+++ FordhamAnalyticsInternship/DataExploration.py
@@ -9,3 +9,10 @@ import os
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 
+#Create path to data files
+path = r'/home/chris/Desktop/ProjectingMajorGrowth/FordhamAnalyticsInternship/RegistrationDatawithEncryptedID-20Years'
+all_files = glob.glob(os.path.join(path, "*.csv"))
+
+#Load and aggregate data files
+df_from_each_file = (pd.read_csv(f, low_memory=False) for f in all_files)
+concatenated_df = pd.concat(df_from_each_file, ignore_index=True)
